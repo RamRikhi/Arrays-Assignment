a6feb390e589e546de640d5ebe6156c022364d56
Ram <jythonr@gmail.com>
2020-05-29 17:56:19 +0530

    commit the right rotating array

--- 004-Arrays-Assigment-2/src/com/rm/leader/LeaderNumber.java
+++ 004-Arrays-Assigment-2/src/com/rm/leader/LeaderNumber.java
@@ -1,14 +1,13 @@
 package com.rm.leader;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class LeaderNumber {
 	public static void main(String[] args) {
 		int[] input = { 1, 6, 6, 5, 7, 4, 1, 7, 7, 7, 7, 7, 7, 7, 2 };
 		System.out.print("Elements are :");
 		for (int i = 0; i < input.length; i++) {
-			System.out.print(input[i]+" ");
+			System.out.print(input[i]+", ");
 		}
 		System.out.println();
 		int[] leader=LeaderNumber.getLeaderElement(input);
@@ -17,18 +16,22 @@ public class LeaderNumber {
 	}
 
 	public static int[] getLeaderElement(int[] input) {
-		int len = input.length;
-		int temp=0;
-		int[] arr =new int[100];
-		len=len/2;
-		int count = 0;
+		int[] temp=new int[input.length];
+		int temp2=0,temp3=0,count=0,size=0;
+		//System.out.println(temp.length);
+		//System.out.println(input.length);
 		for (int i = 0; i < input.length; i++) {
-			for (int j = i+1; j < input.length; j++) {
-				if(input[i]==input[j]) {
-					arr[count++]=input[i];
+			temp2=input[i];
+			for (int j = 0; j < input.length; j++) {
+				if(i!=j) {
+					temp3=input[j];
+					if(temp2==temp3) {
+						++count;
+					}	
 				}
+				System.out.println(count);
 			}
 		}
-		return arr;
+		return temp;
 	}
 }
--- 004-Arrays-Assigment-2/src/com/rm/rightRotate/RightRotateArray.java
+++ 004-Arrays-Assigment-2/src/com/rm/rightRotate/RightRotateArray.java
@@ -4,14 +4,31 @@ import java.util.Arrays;
 
 public class RightRotateArray {
 	public static void main(String[] args) {
-		int arr[]= {1,2,3,4,5};
-		int postion=2;
-		System.out.println("Before left Rotation::"+Arrays.toString(arr));
+		int arr[]= {1,2,3,4,5,10,40,6,8,9};
+		int postion=4;
+		System.out.println("Before right Rotation::"+Arrays.toString(arr));
 		int rotate[]=rightRotateArray(arr,postion);
-		System.out.println("After left Rotation::"+Arrays.toString(rotate));
+		System.out.println("After right Rotation::"+Arrays.toString(rotate));
 	}
 
 	public static int[] rightRotateArray(int[] arr, int postion) {
-		return null;
+		int[] temp =new int[arr.length];
+		int index2 = arr.length-postion;
+		int index = 0;
+		//Shifting right to left side
+		for (int i = index2; i < arr.length; i++) {
+			temp[index++]=arr[i];
+		}
+	
+		index2=0;
+		//shift the remaining array element to left right side
+		for (int i = index; i < temp.length; i++) {
+			temp[i]=arr[index2++];
+		}
+		//copying the temp array to arr array
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=temp[i];
+		}
+		return arr;
 	}
 }
