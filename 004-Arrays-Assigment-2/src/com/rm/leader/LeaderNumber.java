c4fa561eeb97c7e7a54ea00e9d10f159a2c5f532
Ram <jythonr@gmail.com>
2020-05-30 10:55:27 +0530

    Leader Element commit

--- 004-Arrays-Assigment-2/src/com/rm/leader/LeaderNumber.java
+++ 004-Arrays-Assigment-2/src/com/rm/leader/LeaderNumber.java
@@ -1,37 +1,39 @@
 package com.rm.leader;
 
-import java.util.Arrays;
-
 public class LeaderNumber {
 	public static void main(String[] args) {
 		int[] input = { 1, 6, 6, 5, 7, 4, 1, 7, 7, 7, 7, 7, 7, 7, 2 };
+		//Printing the array before searching the leader element
 		System.out.print("Elements are :");
 		for (int i = 0; i < input.length; i++) {
 			System.out.print(input[i]+", ");
 		}
 		System.out.println();
-		int[] leader=LeaderNumber.getLeaderElement(input);
-		System.out.println(Arrays.toString(leader));
+		//method calling 
+		String leader=LeaderNumber.getLeaderElement(input);
+		//printing the leader element
+		System.out.println(leader);
 
 	}
 
-	public static int[] getLeaderElement(int[] input) {
-		int[] temp=new int[input.length];
-		int temp2=0,temp3=0,count=0,size=0;
-		//System.out.println(temp.length);
-		//System.out.println(input.length);
+	public static String getLeaderElement(int[] input) {
+		int count=0,temp=0,holder=0,temp2=0,len=0;
+		//here searching the leader element
 		for (int i = 0; i < input.length; i++) {
-			temp2=input[i];
 			for (int j = 0; j < input.length; j++) {
-				if(i!=j) {
-					temp3=input[j];
-					if(temp2==temp3) {
-						++count;
-					}	
+				if(input[i]==input[j]) {
+					count++;
 				}
-				System.out.println(count);
+				if(count>temp) {
+					holder=input[i];
+					temp2=count;
+				}
 			}
+			temp=count;
+			count=0;
 		}
-		return temp;
+		len=input.length;
+		len=len/2;
+		return (temp2>len) ? "The leader element is:: "+holder:"No leader element found";
 	}
 }
