5630032c6c2a95da1ce57d88c962d36d3c49c15d
Ram <jythonr@gmail.com>
2020-05-30 09:35:32 +0530

    Some changes commit

--- 004-Arrays-Assigment-2/src/com/rm/binary/BinaryArray.java
+++ 004-Arrays-Assigment-2/src/com/rm/binary/BinaryArray.java
@@ -11,24 +11,16 @@ public class BinaryArray {
 	}
 
 	public static int[] binary(int[] input) {
-		int[] temp=new int[input.length];
-		int[] temp2=new int[input.length];
-		int size=0,size2=0;
-		int count=0;
-		for (int i = 0; i < input.length; i++) {
-			if(0==input[i]) {
-				temp[size++]=input[i];
-				count++;
-			}else if(1==input[i]) {
-				temp2[size2++]=input[i];
+	
+		int temp=input[0];
+		int temp2=0;
+		for (int i = 1; i < input.length; i++) {
+			if(temp<=input[i]) {
+				temp2=temp;
+				temp=input[i];
+				input[i]=temp2;
+				
 			}
-		}
-		for (int i = 0; i < input.length; i++) {
-			input[i]=temp[i];
-		}
-		size=0;
-		for (int i = count; i < input.length; i++) {
-			input[i]=temp2[size++];
 		}
 		return input;
 	}
